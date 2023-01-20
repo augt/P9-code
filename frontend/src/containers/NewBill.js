c2ebc729ca639ef8eddb49a11c380b77df8e6d87
Augustin Tacquet <augu.tacquet@gmail.com>
2023-01-20 16:22:03 +0100

    disallow non picture files

--- frontend/src/containers/NewBill.js
+++ frontend/src/containers/NewBill.js
@@ -20,25 +20,33 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    const splitFilename = fileName.split(".");
+    const fileExtension = splitFilename[splitFilename.length-1]
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    if (fileExtension === "png" || fileExtension === "jpg" || fileExtension === "jpeg"){
+      const formData = new FormData()
+          const email = JSON.parse(localStorage.getItem("user")).email
+          formData.append('file', file)
+          formData.append('email', email)
+
+          this.store
+            .bills()
+            .create({
+              data: formData,
+              headers: {
+                noContentType: true
+              }
+            })
+            .then(({fileUrl, key}) => {
+              console.log(fileUrl)
+              this.billId = key
+              this.fileUrl = fileUrl
+              this.fileName = fileName
+            }).catch(error => console.error(error))
+    } else {
+      alert(`Seules les images avec une extension ".jpg", ".jpeg" ou ".png" peuvent être renseignées.`)
+    }
+    
   }
   handleSubmit = e => {
     e.preventDefault()
